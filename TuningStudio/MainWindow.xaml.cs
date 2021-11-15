b6106fb779ae705079f426775e953280fcfdfe3e
Яппаров Дияр Рафаилевич <211925@mck-ktits.ru>
2021-11-15 20:04:37 +0300

    Добавьте файлы проекта.

---
+++ TuningStudio/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TuningStudio.DB;
+using TuningStudio.Pages;
+
+namespace TuningStudio
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Yapparov321AtelyeNightVisionEntities db = new Yapparov321AtelyeNightVisionEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Content = new AutorizationPage();
+        }
+    }
+}
---
+++ TuningStudio/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TuningStudio.DB;
+
+namespace TuningStudio.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddUserButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if(NewFirstNameTB.Text == "" || NewLastNameTB.Text == "" || NewPhoneNumberTB.Text == "" || NewLoginTB.Text == "" || NewPasswordPB.Password.Trim() == "")
+            {
+                MessageBox.Show("Enter the data in full");
+            }
+            else
+            {
+                Client newClient = new Client();
+
+                Autorization newUser = new Autorization();
+                string newLogin = NewLoginTB.Text;
+            
+                foreach(var login in MainWindow.db.Autorization)
+                {
+                    if (NewLoginTB.Text.Trim() == login.Login)
+                    {
+                        MessageBox.Show("The login already exist");
+                    }
+                    else if(NewLoginTB.Text.Trim() != login.Login)
+                    {
+                        newClient.FirstName = NewFirstNameTB.Text;
+                        newClient.LastName = NewLastNameTB.Text;
+                        newClient.PhoneNumber = NewPhoneNumberTB.Text;
+                        newUser.Login = newLogin;
+                        newUser.Password = NewPasswordPB.Password.Trim();
+                        newUser.RoleID = 2;
+                        newUser.ClientID = newClient.ID;
+                        MainWindow.db.Autorization.Add(newUser);
+                        MainWindow.db.Client.Add(newClient);
+                        //MainWindow.db.SaveChanges();
+                        MessageBox.Show("The registration is successful");
+                        this.NavigationService.GoBack();
+                    }
+                }
+            }         
+        }
+    }
+}
