fc06b5aff961f05b1bcd7f87582c6a1c8ea9c153
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2021-11-20 13:10:22 +0300

    added brandschanging button for admin

--- TuningStudio/Pages/AddBrandPage.xaml.cs
+++ TuningStudio/Pages/AddBrandPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TuningStudio.DB;
 
 namespace TuningStudio.Pages
 {
@@ -23,6 +24,42 @@ namespace TuningStudio.Pages
         public AddBrandPage()
         {
             InitializeComponent();
+        }
+
+        private void CountryCB_Loaded(object sender, RoutedEventArgs e)
+        {
+            CountryCB.ItemsSource = MainWindow.db.Country;
+            CountryCB.DisplayMemberPath = "NameOfCountry";
+        }
+
+        private void AddBrandBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Brand newBrand = new Brand();
+            Model newModel = new Model();
+            Body newBody = new Body();
+
+            var selectedCountry = CountryCB.SelectedItem as Country;
+
+            newBrand.NameOfBrand = NewBrandTB.Text;
+            newBrand.CountryID = selectedCountry.ID;
+
+            newModel.NameOfModel = NewModelTB.Text;
+            newModel.BrandID = newBrand.ID;
+
+            newBody.NameOfBody = NewBodyTB.Text;
+            newBody.ModelID = newModel.ID;
+
+            MainWindow.db.Brand.Add(newBrand);
+            MainWindow.db.Model.Add(newModel);
+            MainWindow.db.Body.Add(newBody);
+            MainWindow.db.SaveChanges();
+            MessageBox.Show("Changes are successfuly saved");
+            this.NavigationService.GoBack();
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
         }
     }
 }
